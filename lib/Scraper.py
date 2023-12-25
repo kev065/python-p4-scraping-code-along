384debe77890f15413e744fa4bab76b06d2ae054
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-25 21:54:08 +0300

    fixed TypeError in Scraper class
    modified the Scraper class to create an instance before calling the get_page method. This resolves the TypeError that was occurring when the method was called directly on the class. Also, ensured the Course class is correctly imported from the Course module. This prevents a TypeError when creating a new Course instance

--- lib/Scraper.py
+++ lib/Scraper.py
@@ -1,8 +1,29 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
 import requests
-from Course import Course
 import ipdb
-
+from Course import Course
+import json
 
 class Scraper:
-    pass
+    def __init__(self):
+        self.courses = []
+
+    def get_page(self):
+        doc =  BeautifulSoup(requests.get("http://learn-co-curriculum.github.io/site-for-scraping/courses").text, 'html.parser')
+
+        for course in doc.select('.post'):
+            print(type(course))
+
+            title = course.select("h2")[0].text if course.select("h2") else ''
+            date = course.select(".date")[0].text if course.select(".date") else ''
+            description = course.select("p")[0].text  if course.select("p") else ''
+
+            new_course = Course(title, date, description)
+            self.courses.append(new_course)
+        ipdb.set_trace()
+
+scraper = Scraper()  # Create an instance of the Scraper class
+scraper.get_page()  # Call the get_page method on the instance
+
+
+    
