636551f21a565dcae8574fb3e77e8f4cf02350ba
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-25 21:02:06 +0300

    fixed TypeError in Course class test
    modified the tests for the Course class to provide all required arguments when creating a new instance. This resolves the TypeError that was occurring when the tests were run. Each test now provides a specific value for the instance variable it's testing and placeholder values for the other variables
    
    all 3 Course tests now passing

--- lib/testing/course_test.py
+++ lib/testing/course_test.py
@@ -3,19 +3,17 @@ from Course import Course
 class Test_Course:
     '''Course in Course.py'''
 
-
-
     def test_title_instance_variable(self):
         "Test setting and getting for title"
-        course = Course(title="Programming Robots for Outer Space")
+        course = Course(title="Programming Robots for Outer Space", schedule="Full-Time", description="A course description")
         assert(course.title == "Programming Robots for Outer Space")
 
     def test_schedule_instance_variable(self):
         "Test setting and getting for schedule"
-        course = Course(schedule="Full-Time")
+        course = Course(title="A course title", schedule="Full-Time", description="A course description")
         assert(course.schedule == "Full-Time")
 
     def test_description_instance_variable(self):
         "Test setting and getting for description"
-        course = Course(description="Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
+        course = Course(title="A course title", schedule="Full-Time", description="Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
         assert(course.description == "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
