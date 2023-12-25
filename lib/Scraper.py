586e09a7e25757d4038dfe3bcd921fcd5848971f
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-25 23:00:24 +0300

    updated Scraper class methods and structure
    modified the Scraper class to include get_courses and make_courses methods. Updated the get_page method to return a BeautifulSoup object. Also, ensured that make_courses method returns the courses list after populating it with Course instances

--- lib/Scraper.py
+++ lib/Scraper.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup 
 import requests
-import ipdb
 from Course import Course
 import json
 
@@ -12,18 +11,26 @@ class Scraper:
         doc =  BeautifulSoup(requests.get("http://learn-co-curriculum.github.io/site-for-scraping/courses").text, 'html.parser')
 
         for course in doc.select('.post'):
-            print(type(course))
-
             title = course.select("h2")[0].text if course.select("h2") else ''
             date = course.select(".date")[0].text if course.select(".date") else ''
             description = course.select("p")[0].text  if course.select("p") else ''
 
             new_course = Course(title, date, description)
             self.courses.append(new_course)
-        ipdb.set_trace()
+        return doc  # Return the BeautifulSoup object
 
-scraper = Scraper()  # Creates an instance of the Scraper class
+    def get_courses(self):
+        return self.courses
+
+    def make_courses(self, doc):
+        for course in doc.select('.post'):
+            title = course.select("h2")[0].text if course.select("h2") else ''
+            date = course.select(".date")[0].text if course.select(".date") else ''
+            description = course.select("p")[0].text  if course.select("p") else ''
+
+            new_course = Course(title, date, description)
+            self.courses.append(new_course)
+        return self.courses
+
+scraper = Scraper()  # Create an instance of the Scraper class
 scraper.get_page()  # Call the get_page method on the instance
-
-
-    
