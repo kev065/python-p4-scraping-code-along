8822c34b873edd176b8d984d437f6236828bbc63
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-25 22:14:12 +0300

    added __str__ method and remove separator line in Course class
    Added a __str__ method to the Course class to provide a string representation of Course instances. This makes it easier to view the data in a Course instance when it's printed. Also removed the '------------------' line from the __str__ method to make the output cleaner and more readable

--- lib/Course.py
+++ lib/Course.py
@@ -7,7 +7,4 @@ class Course:
   def __str__(self):
     output = ''
     output += f'Title: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n'
-    output += '------------------'
     return output
-
-
